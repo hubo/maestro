500def5f582d21fa86e81390f3b198faf7956156
isaacgaretmia <isaacgaretmia@gmail.com>
2013-07-10 11:25:41 -0400

    Forgot a critical portion of my checks.

--- install/install-fuerte.sh
+++ install/install-fuerte.sh
@@ -34,6 +34,62 @@ DEPENDENCY_FILES="~/.bashrc"
 BLACKLISTED_DIRS="/opt/ros/fuerte/stacks/armnavigation"
 BLACKLISTED_FILES=""
 
+function see() {
+        if [[ $# != 2 ]]; then
+                return $WRONG_NUMBER_ARGUMENTS
+        fi
+
+        if [[ "$1" == "file" ]]; then
+                if [[ ! -e "$2" || ! -f "$2" ]]; then
+                        return $NOT_FOUND
+                fi
+
+        elif [[ "$1" == "dir" ]]; then
+                if [[ ! -e "$2" || ! -d "$2" ]]; then
+                        return $NOT_FOUND
+                fi
+        else
+                return $BAD_ARGUMENTS
+        fi
+
+        return 0
+}
+
+for dir in $DEPENDENCY_DIRS; do
+        see "dir" "$dir"
+        retval=$?
+        if [[ "$retval" != 0 ]]; then
+                echo "Required dependency $dir was found."
+                exit $NOT_FOUND
+        fi
+done
+
+for file in $DEPENDENCY_FILES; do
+        see "file" "$file"
+        retval=$?
+        if [[ "$retval" != 0 ]]; then
+                echo "Required dependency $file was not found."
+                exit $NOT_FOUND
+        fi
+done
+
+for dir in $BLACKLISTED_DIRS; do
+        see "dir" "$dir"
+        retval=$?
+        if [[ "$retval" == 0 ]]; then
+                echo "Blacklisted directory $dir was found."
+                exit $BLACKLIST_VIOLATED
+        fi
+done
+
+for file in $BLACKLISTED_FILES; do
+        see "file" "$file"
+        retval=$?
+        if [[ "$retval" == 0 ]]; then
+                echo "Blacklisted file $file was found."
+                exit $BLACKLIST_VIOLATED
+        fi
+done
 
 installDir=`pwd`
 
